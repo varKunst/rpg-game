18feee8024805691d126691d5d62b48013ab615c
a_Volunteers <silky_slasher_0w@icloud.com>
2023-03-16 18:57:45 +0900

    Add sorting ordered by name, level, hp, att, def and isParty

--- rpg_guide/src/day04_rpg/Guild.java
+++ rpg_guide/src/day04_rpg/Guild.java
@@ -63,7 +63,7 @@ public class Guild {
 		this.guildList.get(num).printEquitedItem();
 	}
 
-	public void buyUnit() {
+	private void buyUnit() {
 		if (Player.getMoney() < 5000)
 			return;
 		
@@ -104,10 +104,13 @@ public class Guild {
 		this.guildList.add(unit);
 	}
 
-	public void removeUnit() {
+	private void removeUnit() {
 		printAllUnitStaus();
 		System.out.println("삭제할 번호를 입력하세요 ");
 		int sel = MainGame.scan.nextInt();
+		if(sel<1 || sel>this.guildList.size())
+			return;
+		
 		if (this.guildList.get(sel - 1).isParty()) {
 			System.out.println("파티에 참여 중인 멤버는 삭제할수 없습니다.");
 		} else {
@@ -146,27 +149,27 @@ public class Guild {
 			System.out.println(" / " + this.partyList[i].getMaxHp() + "]");
 			System.out.print("[공격력 : " + this.partyList[i].getAtt() + "]");
 			System.out.print(" [방어력 : " + this.partyList[i].getDef() + "]");
-			System.out.println(" [파티중 : " + this.guildList.get(i).isParty() + "]");
+			System.out.println(" [파티중 : " + this.partyList[i].isParty() + "]");
 			System.out.println("");
 		}
 		System.out.println("=====================================");
 	}
 
-	public void partyChange() {
+	private void partyChange() {
 		printParty();
 		System.out.println("교체할 번호를 입력하세요 ");
 		int partyNum = MainGame.scan.nextInt();
-		if(partyNum<1 || partyNum>=this.partyList.length)
+		if(partyNum<1 || partyNum>this.partyList.length)
 			return;
 		
 		printAllUnitStaus();
 		System.out.println("참가할 번호를 입력하세요 ");
 		int guildNum = MainGame.scan.nextInt();
-		if(guildNum<1 || guildNum>=this.guildList.size())
+		if(guildNum<1 || guildNum>this.guildList.size())
 			return;
 		
-		if(this.partyList[partyNum-1].isParty()) {
-			System.out.printf("%s은(는) 이미 파티에 참여중입니다.\n",this.partyList[partyNum-1].getName());
+		if(this.guildList.get(guildNum-1).isParty()) {
+			System.out.printf("%s은(는) 이미 파티에 참여중입니다.\n",this.guildList.get(guildNum-1).getName());
 			return;
 		}
 
@@ -193,6 +196,117 @@ public class Guild {
 			e.printStackTrace();
 		}
 	}
+	
+	private void sortMembers() {
+		System.out.println("정렬 기준을 선택하세요(내림차순).");
+		System.out.println("[1.이름] [2.레벨] [3.체력] [4.공격력] [5.방어력] [6.파티참여]\n[0.뒤로가기]");
+		int sel = MainGame.scan.nextInt();
+		
+		if (sel == 1) {
+			sortByName();
+		} else if (sel == 2) {
+			sortByLevel();
+		} else if (sel == 3) {
+			sortByHp();
+		} else if (sel == 4) {
+			sortByAtt();
+		} else if (sel == 5) {
+			sortByDef();
+		} else if (sel == 6) {
+			sortByParty();
+		}  
+		else if (sel == 0) {
+			return;
+		}
+		
+	}
+	
+	private void sortByName() {
+		for(int i=0; i<this.getListSize(); i++) {
+			for(int j=i; j<this.getListSize(); j++) {
+				String prevName = this.guildList.get(i).getName();
+				String nextName = this.guildList.get(j).getName(); 
+				if(nextName.compareTo(prevName)<0) {
+					Unit temp = this.guildList.get(i);
+					this.guildList.set(i, this.guildList.get(j));
+					this.guildList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	private void sortByLevel() {
+		for(int i=0; i<this.getListSize(); i++) {
+			for(int j=i; j<this.getListSize(); j++) {
+				int prevLevel = this.guildList.get(i).getLevel();
+				int nextLevel = this.guildList.get(j).getLevel(); 
+				if(nextLevel<prevLevel) {
+					Unit temp = this.guildList.get(i);
+					this.guildList.set(i, this.guildList.get(j));
+					this.guildList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	private void sortByHp() {
+		for(int i=0; i<this.getListSize(); i++) {
+			for(int j=i; j<this.getListSize(); j++) {
+				int prevMaxHP = this.guildList.get(i).getMaxHp();
+				int nextMaxHP = this.guildList.get(j).getMaxHp(); 
+				if(nextMaxHP>prevMaxHP) {
+					Unit temp = this.guildList.get(i);
+					this.guildList.set(i, this.guildList.get(j));
+					this.guildList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	private void sortByAtt() {
+		for(int i=0; i<this.getListSize(); i++) {
+			for(int j=i; j<this.getListSize(); j++) {
+				int prevAtt = this.guildList.get(i).getAtt();
+				int nextAtt = this.guildList.get(j).getAtt(); 
+				if(nextAtt>prevAtt) {
+					Unit temp = this.guildList.get(i);
+					this.guildList.set(i, this.guildList.get(j));
+					this.guildList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	private void sortByDef() {
+		for(int i=0; i<this.getListSize(); i++) {
+			for(int j=i; j<this.getListSize(); j++) {
+				int prevDef = this.guildList.get(i).getDef();
+				int nextDef = this.guildList.get(j).getDef(); 
+				if(nextDef>prevDef) {
+					Unit temp = this.guildList.get(i);
+					this.guildList.set(i, this.guildList.get(j));
+					this.guildList.set(j, temp);
+				}
+			}
+		}
+	}
+
+	private void sortByParty() {
+		ArrayList<Unit> temp = new ArrayList<Unit>();
+		
+		for(int i=0; i<this.getListSize(); i++) {
+			if(this.guildList.get(i).isParty()) {
+				temp.add(this.guildList.get(i));
+			}
+		}
+		
+		for(int i=0; i<this.getListSize(); i++) {
+			if(!this.guildList.get(i).isParty())
+				temp.add(this.guildList.get(i));
+		}
+		
+		this.guildList = temp;
+	}
 
 	public void guildMenu() {
 		while (true) {
@@ -207,7 +321,10 @@ public class Guild {
 				removeUnit();
 			} else if (sel == 4) {
 				partyChange();
-			} else if (sel == 0) {
+			} else if (sel == 5) {
+				sortMembers();
+			} 
+			else if (sel == 0) {
 				break;
 			}
 		}
